b5d7f36cc9f985f5627dd8fbb41fb8d8121d332f
junhg0211 <junhg0211@gmail.com>
2021-04-02 07:17:45 +0900

    load extension debugged

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ async def on_ready():
     print(f'{bot.user.display_name} is on')
 
 
-bot.load_extension('cog/music_cog')
-bot.load_extension('cog/test_cog')
+bot.load_extension('cog.music_cog')
+bot.load_extension('cog.test_cog')
 
 bot.run(os.environ['BOT_TOKEN'])
