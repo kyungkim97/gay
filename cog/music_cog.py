fd27d55ce579ae7840cf07cb4200666ed5476795
kyungkim97 <kyungkim97@gmail.com>
2021-04-05 17:53:17 +0900

    play command construct

--- cog/music_cog.py
+++ cog/music_cog.py
@@ -1,3 +1,5 @@
+import youtube_dl as youtube_dl
+from discord import FFmpegPCMAudio
 from discord.ext.commands import Cog, Context, Bot
 from discord.ext import commands
 
@@ -11,6 +13,30 @@ class MusicCog(Cog):
         if ctx.author.voice is not None:
             await ctx.author.voice.channel.connect()
             await ctx.send('접속함')
+
+    @commands.command()
+    async def play(self, ctx: Context, url: str):
+        voice = None
+        for vc in self.bot.voice_clients:
+            if vc.guild == ctx.guild:
+                voice = vc
+                break
+
+        option = {
+            'outtmpl_': 'file/' + url.split('=')[1] + '.mp3'
+        }
+
+        with youtube_dl.YoutubeDL(option) as ydl:
+            ydl.download(url)
+            info = ydl.extract_info(url, download=False)
+            title = info["title"]
+
+        if voice is not None:
+            voice.play(FFmpegPCMAudio("file/" + url.split('=')[1] + ".mp3"))
+            await ctx.send(title + "is playing now")
+        else:
+            await ctx.send('오류가발생했음')
+
 
     @commands.command()
     async def leave(self, ctx: Context):
