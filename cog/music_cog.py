8a8daf7dd0834742159800d6416210d92357cd4c
kyunghwan <kyungkim97@gmail.com>
2021-04-07 17:45:02 +0900

    대충 기본 완성

--- cog/music_cog.py
+++ cog/music_cog.py
@@ -1,7 +1,25 @@
+from urllib.parse import parse_qs, urlparse
+
 import youtube_dl as youtube_dl
 from discord import FFmpegPCMAudio
 from discord.ext.commands import Cog, Context, Bot
 from discord.ext import commands
+
+
+def extract_video_id(url):
+    # Examples:
+    # - http://youtu.be/SA2iWivDJiE
+    # - http://www.youtube.com/watch?v=_oPAwA_Udwc&feature=feedu
+    # - http://www.youtube.com/embed/SA2iWivDJiE
+    # - http://www.youtube.com/v/SA2iWivDJiE?version=3&amp;hl=en_US
+    query = urlparse(url)
+    if query.hostname == 'youtu.be': return query.path[1:]
+    if query.hostname in {'www.youtube.com', 'youtube.com'}:
+        if query.path == '/watch': return parse_qs(query.query)['v'][0]
+        if query.path[:7] == '/embed/': return query.path.split('/')[2]
+        if query.path[:3] == '/v/': return query.path.split('/')[2]
+    # fail?
+    return None
 
 
 class MusicCog(Cog):
@@ -23,20 +41,24 @@ class MusicCog(Cog):
                 break
 
         option = {
-            'outtmpl_': 'file/' + url.split('=')[1] + '.mp3'
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+            'outtmpl': 'file/' + extract_video_id(url)
         }
 
         with youtube_dl.YoutubeDL(option) as ydl:
-            ydl.download(url)
+            ydl.download([url])
             info = ydl.extract_info(url, download=False)
             title = info["title"]
 
         if voice is not None:
-            voice.play(FFmpegPCMAudio("file/" + url.split('=')[1] + ".mp3"))
+            voice.play(FFmpegPCMAudio("file/" + extract_video_id(url) + ".mp3"))
             await ctx.send(title + "is playing now")
         else:
             await ctx.send('오류가발생했음')
-
 
     @commands.command()
     async def leave(self, ctx: Context):
